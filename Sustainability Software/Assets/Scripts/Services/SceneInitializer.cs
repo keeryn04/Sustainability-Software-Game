c6043c36f74d43a2bf5c8ee6658e5ba5ca7bef2c
keeryn04 <keerynjohnson@gmail.com>
2025-11-24 10:34:56 -0700

    (Fix) Developer Talking Correctly, Buttons Linked Correctly

--- Sustainability Software/Assets/Scripts/Services/SceneInitializer.cs
+++ Sustainability Software/Assets/Scripts/Services/SceneInitializer.cs
@@ -50,6 +50,8 @@ public class SceneInitializer : MonoBehaviour
         {
             SlideshowViewer.Instance.SetSlideshow(MenuManager.Instance.PendingLearnType);
         }
+
+        DialogueManager.Instance.AssignDeveloperUI(typingAnimator, audioSource);
     }
 
     private void SetupPlayingScene()
@@ -61,10 +63,10 @@ public class SceneInitializer : MonoBehaviour
                 objectiveText,
                 scoreText,
                 choiceButtons,
-                resourceBar,
-                typingAnimator,
-                audioSource
+                resourceBar
             );
+
+            DialogueManager.Instance.AssignDeveloperUI(typingAnimator, audioSource);
         }
 
         if (GameManager.Instance != null)
